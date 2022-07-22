aff8bf7779d75a5cc84b57a64e208cf602cfbfee
Matt Pittenger <pittengermdp@gmail.com>
2022-07-22 07:18:11 -0500

    Got stock price back

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-#![deny(warnings)]
-#![warn(clippy::all, clippy::restriction, clippy::pedantic, clippy::nursery)]
-use influxdb2::write::WriteQuery;
-use influxdb2::models::Query;
-use influxdb2::Client;
-use influxdb_line_protocol::{Field, Point};
-use std::env;
-use yahoo_finance::{history, Interval};
+//#![deny(warnings)]
+//#![warn(clippy::all, clippy::restriction, clippy::pedantic, clippy::nursery)]
 use anyhow::{Context, Result};
 use chrono::NaiveDateTime;
 use clap::{Parser, Subcommand};
+//use influxdb2::models::Query;
+use influxdb2::Client;
 use influxdb2_structmap::FromMap;
-
-
+use std::env;
+use yahoo_finance::{history, Interval};
+use futures::prelude::*;
+use influxdb2::models::DataPoint;
+use influxdb2::models::Query;
+use yahoo_finance::Timestamped;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
@@ -40,23 +40,38 @@ enum Operation {
 }
 
 #[derive(Debug, influxdb2_structmap_derive::FromMap, Default)]
-struct Test(f32);
+pub struct StockPrice {
+   ticker: String,
+   value: f64,
+   time: i64,
+}
 
 async fn query_database(
     client: Client,
     op: &Operation,
+    bucket: &str,
     symbol: &str,
     start_date: NaiveDateTime,
     end_date: NaiveDateTime,
-) -> Result<f32, Box<dyn std::error::Error>> {
+) -> Result<f64, Box<dyn std::error::Error>> {
+   let qs = format!("from(bucket: \"test1\") 
+   |> range(start: -1w)
+   |> filter(fn: (r) => r.ticker == \"{}\") 
+   |> last()
+", "AAPL");
+/*
     let mut qs = format!(
-        "from(bucket: \"stock-prices\") 
+        "from(bucket: {})
         |> range(start: {}, end: {})
-        |> filter(fn: (r) => r.ticker == \"{}\")",
+        |> filter(fn: (r) => r.symbol == \"{}\")",
+         bucket,
         symbol,
         start_date.timestamp_millis(),
         end_date.timestamp_millis()
     );
+    
+    println!("Query suggestions are: {:#?}", client.query_suggestions().await?);
+    
     match op {
         Operation::Max => {
             println!(
@@ -72,47 +87,61 @@ async fn query_database(
             );
             qs.push_str("|> min()");
         }
-    }
+    }*/
     let query = Query::new(qs.to_string());
-    let res: Vec<Test> = client.query(Some(query)).await
-    .with_context(|| format!("failed to query database for {:?} value", op))?;
-    Ok(res[0].0)
+    let res: Vec<StockPrice> = client.query::<StockPrice>(Some(query))
+        .await?;
+   println!("result is: {:?}", res);
+    //Ok(res[0].1.high)
+    Ok(1.0)
 }
 
-fn connect_to_influxdb(host: &str, org: &str, token: &str) -> Result<Client, Box<dyn std::error::Error>> {
-   Ok(Client::new(host, org.clone(), token)?)
+
+fn connect_to_influxdb(
+    host: &str,
+    org: &str,
+    token: &str,
+) -> Result<Client, Box<dyn std::error::Error>> {
+    Ok(Client::new(host, org.clone(), token))
 }
 
-async fn get_symbol_data(symbol: &str, interval: Interval) -> Result<Vec<yahoo_finance::Bar>, Box<dyn std::error::Error>> {
-   Ok(history::retrieve_interval(symbol, interval).await?)
+async fn get_symbol_data(
+    symbol: &str,
+    interval: Interval,
+) -> Result<Vec<yahoo_finance::Bar>, Box<dyn std::error::Error>> {
+    Ok(history::retrieve_interval(symbol, interval).await?)
 }
 
-async fn get_symbol_list_data(symbols: &[String], intervals: &[Interval]) -> Result<Vec<Vec<yahoo_finance::Bar>>, Box<dyn std::error::Error>> {
-   let mut symbols_data: Vec<Vec<yahoo_finance::Bar>> = Vec::with_capacity(symbols.len());
-   for (symbol, interval) in symbols.iter().zip(intervals) {
-      symbols_data.push(get_symbol_data(symbol, *interval).await?)
-   }
-   Ok(symbols_data)
+async fn get_symbol_list_data(
+    symbols: &[String],
+    interval: Interval,
+) -> Result<Vec<Vec<yahoo_finance::Bar>>, Box<dyn std::error::Error>> {
+    let mut symbols_data: Vec<Vec<yahoo_finance::Bar>> = Vec::with_capacity(symbols.len());
+    for symbol in symbols.iter() {
+        symbols_data.push(get_symbol_data(symbol, interval).await?)
+    }
+    Ok(symbols_data)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-   let args = Cli::parse();
+   
+    let args = Cli::parse();
 
-   let start_date =
-       NaiveDateTime::parse_from_str(&args.start, "%Y-%m-%d %H:%M:%S").with_context(|| {
-           format!(
-               "could not convert {:?} to YYYY-MM-DD HH:MM:SS format",
-               &args.start
-           )
-       })?;
-   let end_date =
-       NaiveDateTime::parse_from_str(&args.end, "%Y-%m-%d %H:%M:%S").with_context(|| {
-           format!(
-               "could not convert {:?} to YYYY-MM-DD HH:MM:SS format",
-               &args.end
-           )
-       })?;
+    let start_date =
+        NaiveDateTime::parse_from_str(&args.start, "%Y-%m-%d %H:%M:%S").with_context(|| {
+            format!(
+                "could not convert {:?} to YYYY-MM-DD HH:MM:SS format",
+                &args.start
+            )
+        })?;
+    let end_date =
+        NaiveDateTime::parse_from_str(&args.end, "%Y-%m-%d %H:%M:%S").with_context(|| {
+            format!(
+                "could not convert {:?} to YYYY-MM-DD HH:MM:SS format",
+                &args.end
+            )
+        })?;
 
     let host = env::var("INFLUX_HOST").unwrap_or("http://localhost:8086".into());
     let org = env::var("INFLUX_ORG").unwrap_or("example-org".into());
@@ -120,33 +149,47 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "iPRI-RQKTiCjnjXV3lFaMbnR8lg3h43-HmHjlZkKYF1XzKdviyl4sOP2xl_h53YOSDaUIhi0NNdZDqYTrXpqXw=="
             .into(),
     );
-    let bucket = env::var("INFLUX_BUCKET").unwrap_or("test".into());
+    let bucket = env::var("INFLUX_BUCKET").unwrap_or("test1".into());
 
     let client = connect_to_influxdb(&host, &org, &token)?;
-    let interval = vec![Interval::_6mo];
-    let symbol = vec!["AAPL".to_string()];
-    let data_vec = get_symbol_list_data(&symbol, &interval).await?;
+    let interval = Interval::_6mo;
+    let symbols = vec!["AAPL".to_string()];
+    let data_vec = get_symbol_list_data(&symbols, interval).await?;
+    let mut symbol_iter = symbols.iter();
+    
+    println!("HealthCheck: {:#?}", client.health().await?);
+    /*
+    for bar_vec in &data_vec {
+      let mut points: Vec<DataPoint> = Vec::with_capacity(bar_vec.len()); 
+      let symbol = symbol_iter.next();
+      println!("Number of symbols in data_vec is {}", &data_vec.len());
 
-    for bar_vec in data_vec {
       for bar in bar_vec {
-         let fields = vec![
-            Field::new("open", bar.open).expect("Failed to create open field"),
-            Field::new("high", bar.high).expect("Failed to create high field"),
-            Field::new("low", bar.low).expect("Failed to create low field"),
-            Field::new("close", bar.close).expect("Failed to create close field"),
-            Field::new("volume", bar.volume.expect("No Volume Data"))
-                .expect("Failed to create volume field"),
-        ];
-        let point = Point::builder(bar.timestamp.to_string())
-            .unwrap()
-            .add_fields(fields)
-            .build()
-            .unwrap();
-
-        let query = WriteQuery::with_org(&bucket, &org);
-        let _result = client.clone().write(point, query).await?;
-      }        
-    }
+         points.push(
+             DataPoint::builder("bar")
+                 //bar timestamp is in millis, influxdb stores timestamp as nano
+                 .timestamp(bar.timestamp_millis() * 1_000_000)
+                 .tag("symbol", symbol.expect("No symbol provided"))
+                 .field("open", bar.open)
+                 .field("high", bar.high)
+                 .field("low", bar.low)
+                 .field("close", bar.close)
+                 .field("volume", bar.volume.unwrap_or_default() as i64)
+                 .build()?,
+         );
+     }
+      println!("Writing {} bars", points.len());
+      */
+      let points: Vec<DataPoint> = vec![ DataPoint::builder("bar")
+      //bar timestamp is in millis, influxdb stores timestamp as nano
+      .tag("ticker", "AAPL")
+      .field("time", 0)
+      .field("value", 123.45)
+      .build()?, ];
+      client.write(&bucket, stream::iter(points)).await?;
+    //}
+    let max = query_database(client, &args.op, &bucket, &args.symbol, start_date, end_date).await?;
+    println!("Max price in interval is: {}", max);
 
     Ok(())
 }
